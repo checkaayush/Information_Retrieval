eb15ab804d3b213f82254ef923a948b9eb946797
Aayush Sarva <aayush@socialcops.org>
2015-08-26 18:13:51 +0530

    Added TF-IDF weight matrix calculation

--- IRWS_Assignment_1/similarity_measures.py
+++ IRWS_Assignment_1/similarity_measures.py
@@ -8,6 +8,7 @@ import subprocess
 import urllib
 import glob
 import numpy
+from math import log10
 
 from nltk.corpus import stopwords
 from google import search
@@ -32,7 +33,7 @@ def download_documents(query):
         # Cleaning the encoded URL for filename
         filename = urllib.unquote(url).split('/')[-1]
 
-        print "Downloading: \nFilename: %s\nURL: %s" % (filename, url)
+        print "\nDownloading: \nFilename: %s\nURL: %s" % (filename, url)
         urllib.urlretrieve(url, filename)
         # print urllib.unquote(url)
 
@@ -121,13 +122,48 @@ def get_termfreq_list(query, filename):
     return tf_list
 
 
+def generate_tf_idf_matrix(tf_matrix):
+    """Generates TF-IDF weight matrix.
+
+    Args:
+        tf_matrix (numpy array): Term frequency matrix
+
+    Returns:
+        numpy array: Required weight matrix
+    """
+    (rows, cols) = tf_matrix.shape
+
+    # Create weight matrix
+    weight_matrix = numpy.zeros(shape=(rows, cols), dtype=numpy.float)
+
+    for i in range(rows):
+        for j in range(cols):
+            tf = tf_matrix[i][j]
+
+            # Calculate document frequency
+            df = 0
+            for entry in tf_matrix[:, j]:
+                # print "entry ", entry
+                if entry > 0:
+                    df += 1
+
+            idf = 1 + log10((10 * NUM_DOCS) / df)
+
+            weight_matrix[i][j] = tf * idf
+
+    return weight_matrix
+
+
 def main():
+
+    # Default query for testing
+    # query = "anna hazare"
 
     query = raw_input("Query: ")
     query = query.lower()
     # download_documents(query)
 
-    # # Generate text files from doc files
+    # Generate text files from doc files
     # doc_files = glob.glob('*.doc')
     # if doc_files:
     #     for f in doc_files:
@@ -135,7 +171,7 @@ def main():
     # else:
     #     print "No doc files to process."
 
-    # # Preprocess text files
+    # Preprocess text files
     # text_files = glob.glob('*.txt')
     # if text_files:
     #     for f in text_files:
@@ -143,6 +179,7 @@ def main():
     # else:
     #     print "No text files to process."
 
+    # Generate Frequency Matrix
     processed_files = glob.glob('processed*.txt')
     num_files = len(processed_files)
     num_terms = len(query.split())
@@ -151,13 +188,16 @@ def main():
     if processed_files:
         index = 0
         for f in processed_files:
-            print f
+            print "\nProcessed file : %s" % f
             tf_matrix[index] = get_termfreq_list(query, f)
             index += 1
     else:
         print "No files to process."
+    print "\nTerm frequency matrix:\n\n", tf_matrix
 
-    print tf_matrix
+    # Calculate TF-IDF weighting
+    weight_matrix = generate_tf_idf_matrix(tf_matrix)
+    print "\nTF-IDF Weight Matrix:\n\n", weight_matrix
 
 if __name__ == "__main__":
     main()
